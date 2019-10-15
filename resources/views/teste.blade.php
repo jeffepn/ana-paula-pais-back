009e804150d3480cd8c9709ea901927c8a4ac72d
Jefferson1001 <jeffersonpuc@gmail.com>
2019-10-15 14:25:32 -0300

    Add images utils immobiles

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -32,4 +32,74 @@
         </div>
     </div>
 </div>
+<div class="px-3 px-md-5 my-5">
+    <h2 class=" ft-secoundary">
+        CARACTERÍSTICA(S)
+    </h2>
+    <p>
+        Onde se encontra supermercado, padaria, restaurante, farmácia, escola, locadora, petshops, entre outros
+        estabelecimentos comercias e de lazer. Próximo à Avenida João Pinheiros, Parque Municipal, entre outros, Jardim
+        Country Club é considerado os bairros mais crescem da Região.
+    </p>
+    <p>
+        O bairro é excelente para quem procura comodidade e conforto.
+    </p>
+    <p>
+        Da estrutura ao acabamento, pensamos em cada detalhe para um maior conforto e praticidade
+    </p>
+    1) Pisos e azulejos em porcelanato Portobello ou similar
+    - O porcelanato é um material com tecnologia mais avançada e, em geral, possui maior resistência e durabilidade,
+    permitindo rejuntes menos espessos.<br>
+
+    2) Isopor nas lajes para melhor isolamento acústico
+    - Leveza, rapidez na montagem e facilidade de instalação de canos e conduítes. O peso pode chegar até 70% menos que
+    o material tradicional. Melhora o desempenho acústico da obra, e térmico da construção.<br>
+
+    3) Estrutura em concreto armado (não tijolo estrutural)
+    - Uma estrutura monolítica, hiperestática e, por esta razão, possui maiores reservas de segurança, manutenção e
+    conservação praticamente nulas, em associação a grande durabilidade. Apresenta resistência e efeitos térmicos,
+    atmosféricos e a desgastes mecânicos.<br>
+
+    4) Molduras em gesso personalizadas
+    - As molduras decorativas em gesso podem ser utilizadas em todos os cômodos, até mesmo em ambientes externos de
+    residências ou estabelecimentos comerciais, pois enriquecem o ambiente.<br>
+
+    5) Rodapés em Porcelanato
+    - Com excelente durabilidade e fácil instalação. Possui um brilho esplêndido, fazendo todo o diferencial no
+    ambiente. Não mofa, não empena, fácil de limpar e anti cupins.<br>
+
+    6) Sistema de interfone com PABX - Comunica entre os apartamentos
+    - Permite acionar as saídas de fechadura pelo telefone do apartamento; portaria programável e comunicação sem
+    ruídos. Universal, pode ser usado em qualquer central padrão ANATEL.<br>
+
+    7) Venezianas e portas balcão em esquadrilhas de alumínio
+    - O alumínio apresenta grande longevidade devido à resistência à corrosão, atributo este que se complementa com os
+    tratamentos de superfície. A baixa manutenção exigida pelos caixilhos de alumínio é um fator econômico que deve ser
+    levado em conta no investimento inicial do imóvel.<br>
+
+    8) Gás Encanado | Aquecimento de água a gás para todo o apartamento (opcional)
+    - A forma mais barata de energia de fornecimento contínuo. O uso do gás encanado pode ser ampliado para os demais
+    pontos da casa, propiciando, entre outros benefícios, água quente para o banho.<br>
+
+    9) Tubulação independente para TV, internet e energia<br>
+
+    10) Sacadas em todos os apartamentos<br>
+
+    11) Gabinetes de banheiro e cozinha planejados na cor branca<br>
+
+    12) Porteiro 24hs<br>
+
+    13) 2 Elevadores<br>
+
+    14) Varanda Gourmet da Cidade<br>
+
+    15) Academia
+    <h2 class="my-3 ft-secoundary">Planta</h2>
+    <h4 class="my-4">Planta 1</h4>
+    <img class="m-w-100" src="/images/utilsimmobiles/planta1vogozeuz.jpg" alt="">
+    <h4 class="my-4">Planta 2</h4>
+    <img class="m-w-100" src="/images/utilsimmobiles/planta2vogozeuz.jpg" alt="">
+    <h4 class="my-4">Planta 3</h4>
+    <img class="m-w-100" src="/images/utilsimmobiles/planta3vogozeuz.jpg" alt="">
+</div>
 @endsection
