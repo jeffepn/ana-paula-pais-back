9c687f43c0169e9e82aff11574e52077ba6889d3
Jefferson1001 <jeffersonpuc@gmail.com>
2019-10-16 08:21:28 -0300

    Add images utils immobiles

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -7,99 +7,57 @@
     <h2 class=" ft-secoundary">
         CARACTERÍSTICA(S)
     </h2>
-    <div class="row ">
-        <div class="col-sm-6 col-md-4">
-            <li>
-                Localização privilegiada
-            </li>
-            <li>
-                Acesso por três endereços
-            </li>
-            <li>
-                Vista panorâmica da serra e do centro desde o primeiro andar
-            </li>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <li>
-                Apartamentos com 2 dormitórios ou duplex com 3 dormitórios
-            </li>
-            <li>
-                Piscina, Academia, Playground, Brinquedoteca e Área Gourmet
-            </li>
-            <li>
-                Todos os apartamentos com varanda gourmet e churrasqueira
-            </li>
-        </div>
-    </div>
+    <h2 class=" ft-secoundary">
+        Condomínio: Vila das Flores
+    </h2>
+    <p>
+        (3) dormitórios: sendo (2) quartos e (1) suíte; (1) banheiro; box no bwc;
+        lavabo; sl. 2 ambientes; cozinha; área de serviço; teto de laje; (1) sala
+    </p>
+    <i class="far fa-check-square mr-2"></i> Asfalto<br>
+    <i class="far fa-check-square mr-2"></i>Calçada<br>
+    <i class="far fa-check-square mr-2"></i>Cerca Elétrica<br>
+    <i class="far fa-check-square mr-2"></i>Interfone<br>
+    <i class="far fa-check-square mr-2"></i>Jardim c/ grama<br>
+    <i class="far fa-check-square mr-2"></i>Portão Eletrônico<br>
 </div>
 <div class="px-3 px-md-5 my-5">
     <h2 class=" ft-secoundary">
         CARACTERÍSTICA(S)
     </h2>
+    <h3 class=" ft-secoundary">
+        A tranquilidade da Rua Castro Alves no centro de Poços de Caldas
+    </h3>
     <p>
-        Onde se encontra supermercado, padaria, restaurante, farmácia, escola, locadora, petshops, entre outros
-        estabelecimentos comercias e de lazer. Próximo à Avenida João Pinheiros, Parque Municipal, entre outros, Jardim
-        Country Club é considerado os bairros mais crescem da Região.
+        Projetado para quem busca praticidade.
+
     </p>
     <p>
-        O bairro é excelente para quem procura comodidade e conforto.
+        A opção perfeita para quem deseja evitar as altas taxas de condomínio. Seja porque os moradores desejam um
+        ambiente mais tranquilo, ou pela falta de tempo em usar as áreas comuns do condomínio, ou até mesmo para
+        economizar.
     </p>
-    <p>
-        Da estrutura ao acabamento, pensamos em cada detalhe para um maior conforto e praticidade
+    <p class="text-uppercase">
+        A COMODIDADE de morar no centro.<br>
+
+        O CONFORTO com espaço e varanda gourmet.<br>
+
+        A CONFIANÇA no padrão de construção e entrega Elos Construtora.<br>
     </p>
-    1) Pisos e azulejos em porcelanato Portobello ou similar
-    - O porcelanato é um material com tecnologia mais avançada e, em geral, possui maior resistência e durabilidade,
-    permitindo rejuntes menos espessos.<br>
+    <h2 class="ft-secoundary text-left
+    ">Um Condomínio Completo</h2>
+    <p class="font-weight-bold">
+        90 m², 3 dormitórios, 2 vagas por apto, varanda gourmet, portaria com segurança
+    </p>
+    <p>Perto de tudo que VOCÊ precisa!</p>
+    <p> Se você procura praticidade, modernidade e conforto, o Residencial Sorano se localiza numa região calma
+        no centro da cidade, perto de tudo que você precisa, com apartamentos de 3 dormitórios.</p>
+    <p>E você pode dar um toque personalizado em seu apartamento com as diferentes opções de acabamento
+        disponíveis.</p>
+    <p>Rua Castro Alves no centro de Poços de Caldas</p>
+    <div class="row">
+        <img class="m-w-100" src="/images/utilsimmobiles/plantasorano.jpg" alt="">
+    </div>
 
-    2) Isopor nas lajes para melhor isolamento acústico
-    - Leveza, rapidez na montagem e facilidade de instalação de canos e conduítes. O peso pode chegar até 70% menos que
-    o material tradicional. Melhora o desempenho acústico da obra, e térmico da construção.<br>
-
-    3) Estrutura em concreto armado (não tijolo estrutural)
-    - Uma estrutura monolítica, hiperestática e, por esta razão, possui maiores reservas de segurança, manutenção e
-    conservação praticamente nulas, em associação a grande durabilidade. Apresenta resistência e efeitos térmicos,
-    atmosféricos e a desgastes mecânicos.<br>
-
-    4) Molduras em gesso personalizadas
-    - As molduras decorativas em gesso podem ser utilizadas em todos os cômodos, até mesmo em ambientes externos de
-    residências ou estabelecimentos comerciais, pois enriquecem o ambiente.<br>
-
-    5) Rodapés em Porcelanato
-    - Com excelente durabilidade e fácil instalação. Possui um brilho esplêndido, fazendo todo o diferencial no
-    ambiente. Não mofa, não empena, fácil de limpar e anti cupins.<br>
-
-    6) Sistema de interfone com PABX - Comunica entre os apartamentos
-    - Permite acionar as saídas de fechadura pelo telefone do apartamento; portaria programável e comunicação sem
-    ruídos. Universal, pode ser usado em qualquer central padrão ANATEL.<br>
-
-    7) Venezianas e portas balcão em esquadrilhas de alumínio
-    - O alumínio apresenta grande longevidade devido à resistência à corrosão, atributo este que se complementa com os
-    tratamentos de superfície. A baixa manutenção exigida pelos caixilhos de alumínio é um fator econômico que deve ser
-    levado em conta no investimento inicial do imóvel.<br>
-
-    8) Gás Encanado | Aquecimento de água a gás para todo o apartamento (opcional)
-    - A forma mais barata de energia de fornecimento contínuo. O uso do gás encanado pode ser ampliado para os demais
-    pontos da casa, propiciando, entre outros benefícios, água quente para o banho.<br>
-
-    9) Tubulação independente para TV, internet e energia<br>
-
-    10) Sacadas em todos os apartamentos<br>
-
-    11) Gabinetes de banheiro e cozinha planejados na cor branca<br>
-
-    12) Porteiro 24hs<br>
-
-    13) 2 Elevadores<br>
-
-    14) Varanda Gourmet da Cidade<br>
-
-    15) Academia
-    <h2 class="my-3 ft-secoundary">Planta</h2>
-    <h4 class="my-4">Planta 1</h4>
-    <img class="m-w-100" src="/images/utilsimmobiles/planta1vogozeuz.jpg" alt="">
-    <h4 class="my-4">Planta 2</h4>
-    <img class="m-w-100" src="/images/utilsimmobiles/planta2vogozeuz.jpg" alt="">
-    <h4 class="my-4">Planta 3</h4>
-    <img class="m-w-100" src="/images/utilsimmobiles/planta3vogozeuz.jpg" alt="">
 </div>
 @endsection
