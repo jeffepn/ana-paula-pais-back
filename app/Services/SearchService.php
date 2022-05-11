d537321242bfd3e05bd225b0b1c138d77ee5034e
JPDevWeb <jefferson.pereira.nascimento10@gmail.com>
2022-05-11 06:46:58 -0300

    Fix order by entities form search

---
+++ app/Services/SearchService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use Jeffpereira\RealEstate\Models\Property\BusinessProperty;
+use Jeffpereira\RealEstate\Models\Property\SubType;
+use JPAddress\Models\Address\Neighborhood;
+
+class SearchService
+{
+
+    public function getBusinesses()
+    {
+        return BusinessProperty::join('properties', 'business_properties.property_id', 'properties.id')
+            ->join('businesses', 'business_properties.business_id', 'businesses.id')
+            ->select('businesses.*')
+            ->orderBy('businesses.name', 'asc')
+            ->distinct()
+            ->get();
+    }
+
+    public function getNeighborhoods()
+    {
+        return Neighborhood::join("addresses", "neighborhoods.id", "=", "addresses.neighborhood_id")
+            ->join('properties', function ($q) {
+                $q->on("properties.address_id", "addresses.id")
+                    ->where("active", true);
+            })
+            ->with('city')
+            ->select("neighborhoods.*")
+            ->orderBy('name', 'asc')
+            ->get()
+            ->unique();
+    }
+
+    public function getSubtypes()
+    {
+        return SubType::whereHas('properties')
+            ->orderBy('name', 'asc')
+            ->get();
+    }
+}
