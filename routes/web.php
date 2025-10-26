edcd83786bddf4c8d50c2b29bb2ed7a4a4953955
jeffepn <jefferson.pereira.nascimento10@gmail.com>
2025-10-26 13:04:09 -0300

    [FEAT] -  remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -14,32 +14,30 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', 'SiteController@home');
-Route::get('nossos-servicos', 'SiteController@services');
-Route::get('sobre', 'SiteController@about');
-Route::get('contato', 'SiteController@contact');
-Route::post('contato', 'SiteController@sendcontact');
-Route::post('newsletter', 'SiteController@newsletter');
+// Route::get('/', 'SiteController@home');
+// Route::get('nossos-servicos', 'SiteController@services');
+// Route::get('sobre', 'SiteController@about');
+// Route::get('contato', 'SiteController@contact');
+// Route::post('contato', 'SiteController@sendcontact');
+// Route::post('newsletter', 'SiteController@newsletter');
 
 //Enterprises
-Route::get('empreendimentos', 'SiteController@enterprises');
-Route::get('empreendimento/{slug}', 'SiteController@enterprise');
+// Route::get('empreendimentos', 'SiteController@enterprises');
+// Route::get('empreendimento/{slug}', 'SiteController@enterprise');
 
-Route::get('blue', 'SiteController@blue');
-Route::get('chateau', 'SiteController@chateau');
-Route::get('easy', 'SiteController@easy');
-Route::get('green', 'SiteController@green');
-Route::get('lumiere', 'SiteController@lumiere');
-Route::get('unique', 'SiteController@unique');
+// Route::get('blue', 'SiteController@blue');
+// Route::get('chateau', 'SiteController@chateau');
+// Route::get('easy', 'SiteController@easy');
+// Route::get('green', 'SiteController@green');
+// Route::get('lumiere', 'SiteController@lumiere');
+// Route::get('unique', 'SiteController@unique');
 
-Route::get('busca-de-imoveis', 'SiteController@searchProperties')->name('property.search_properties');
-Route::post('busca-imovel-por-codigo', 'SiteController@searchPropertyCode')->name('property.search_per_code');
-Route::get('registra-busca-de-imoveis', 'SiteController@setSessionSearch')->name('property.set_filter');
-Route::get('imovel/{slug?}', 'SiteController@property')->name('property.show');
+// Route::get('busca-de-imoveis', 'SiteController@searchProperties')->name('property.search_properties');
+// Route::post('busca-imovel-por-codigo', 'SiteController@searchPropertyCode')->name('property.search_per_code');
+// Route::get('registra-busca-de-imoveis', 'SiteController@setSessionSearch')->name('property.set_filter');
+// Route::get('imovel/{slug?}', 'SiteController@property')->name('property.show');
 
 //Generator sitemap
-
 Auth::routes(['register' => false]);
-
+Route::get('/', fn() => redirect()->route('login'));
 Route::get('/home', 'HomeController@index')->name('home');
